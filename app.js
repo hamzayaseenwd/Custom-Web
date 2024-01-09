4001d604cbea0216a1bde3ea23251e26ee3ede77
hamzayaseenwd <hamzayaseen836@gmail.com>
2024-01-09 21:14:38 +0500

    Update in process

--- app.js
+++ app.js
@@ -1,5 +1,79 @@
-let shopByCategory = [
+let Atar = [
     {
+        image : "images/CategoryImages/Atar/212 men.png",
+        brand : "  ",
+        product : "  ",
+        price : "Rs.800",
+    },
+    {
+        image : "images/CategoryImages/Atar/212 men.png",
+        brand : "  ",
+        product : "  ",
+        price : "Rs.800",
+    },
+    
+]
 
+
+let brandedPocketPerfumes = [
+    {
+        image : "images/CategoryImages/Branded Pocket Perfumes.jpg",
+        brand : " ",
+        product : " ",
+        price : "Rs.800",
+    },
+    {
+        image : "images/CategoryImages/Branded Pocket Perfumes.jpg",
+        brand : " ",
+        product : " ",
+        price : "Rs.800",
+    },
+    {
+        image : "images/CategoryImages/Branded Pocket Perfumes.jpg",
+        brand : " ",
+        product : " ",
+        price : "Rs.800",
+    },
+    {
+        image : "images/CategoryImages/Branded Pocket Perfumes.jpg",
+        brand : " ",
+        product : " ",
+        price : "Rs.800",
+    },
+    {
+        image : "images/CategoryImages/Branded Pocket Perfumes.jpg",
+        brand : " ",
+        product : " ",
+        price : "Rs.800",
+    },
+    {
+        image : "images/CategoryImages/Branded Pocket Perfumes.jpg",
+        brand : " ",
+        product : " ",
+        price : "Rs.800",
+    },
+    {
+        image : "images/CategoryImages/Branded Pocket Perfumes.jpg",
+        brand : " ",
+        product : " ",
+        price : "Rs.800",
+    },
+    {
+        image : "images/CategoryImages/Branded Pocket Perfumes.jpg",
+        brand : " ",
+        product : " ",
+        price : "Rs.800",
+    },
+    {
+        image : "images/CategoryImages/Branded Pocket Perfumes.jpg",
+        brand : " ",
+        product : " ",
+        price : "Rs.800",
+    },
+    {
+        image : "images/CategoryImages/Branded Pocket Perfumes.jpg",
+        brand : " ",
+        product : " ",
+        price : "Rs.800",
     },
 ]
